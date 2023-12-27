0b042593dabd50e67ec21d96a38944bd2ec997a2
AdityaVijay1 <adityavijay1712@gmail.com>
2023-12-27 16:02:10 +0800

    Changes to home page and result page, adding button functionality

--- app/src/main/java/com/example/numbersmemorygamea2/HomeActivity.java
+++ app/src/main/java/com/example/numbersmemorygamea2/HomeActivity.java
@@ -1,8 +1,10 @@
 package com.example.numbersmemorygamea2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,44 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Find buttons by ID
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnHighScores = findViewById(R.id.btnHighScores);
+        Button btnOptions = findViewById(R.id.btnOptions);
+
+        // Set click listeners for each button
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to SignUp Page
+                startActivity(new Intent(HomeActivity.this, SignUpActivity.class));
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to Login Page
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnHighScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to High Score Page
+                startActivity(new Intent(HomeActivity.this, HighScoreActivity.class));
+            }
+        });
+
+        btnOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Redirect to Background Music Page
+                startActivity(new Intent(HomeActivity.this, OptionsActivity.class));
+            }
+        });
     }
 }
